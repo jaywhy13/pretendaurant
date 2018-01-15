bdf1c4364dfbaa18cc27b6d7705fc0cee1864f39
Jean-Mark Wright <jeanmark.wright@gmail.com>
2018-01-15 18:26:29 -0500

    Added view components for customer and staff-member

--- static/js/index.js
+++ static/js/index.js
@@ -2,6 +2,39 @@ function getRandomInt(max){
     var randomNum = 1 + parseInt(Math.floor(Math.random() * max));
     return randomNum;
 }
+
+Vue.component('customer', {
+    props: ['customer'],
+    template: '\
+            \
+            <div>\
+                Customer\
+                    <img v-bind:src="customer.url" width="64px"/>\
+            </div>\
+    ',
+    data: function(){
+        return {}
+    }
+});
+
+Vue.component('staff-member', {
+    props: ['staff'],
+    template: '\
+            <div>\
+                Staff\
+                <img v-bind:src="staff.url" width="128px"/>\
+                <div v-if="staff.customer">\
+                    <img v-bind:src="staff.customer.url" width="56px" />\
+                </div>\
+            </div>\
+    ',
+    data: function(){
+        return {
+            'customer': null,
+        }
+    }
+});
+
 var app = new Vue({
     el: '#app',
     data: {
