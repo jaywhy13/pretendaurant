f8fcdbf41473a2592fe5e6f02a84f50ff1182dfc
Jean-Mark Wright <jeanmark.wright@gmail.com>
2021-08-18 22:35:08 -0400

    Converted App.js to typescript.
    App.tsx now has a basic display that shows each line and the customers assigned
    to each line.

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { useSelector } from 'react-redux';
+import './App.css';
+import { Clock } from './components/Clock';
+import { CustomerInLine } from './components/CustomerInLine';
+import { selectCustomersWaitingToJoinLine, selectLines } from './features/restaraunt/selectors';
+
+function App() {
+  const lines = useSelector(selectLines);
+  const customersWaitingToJoinLine = useSelector(selectCustomersWaitingToJoinLine);
+  return (
+    <div className="App">
+      <Clock />
+      Everything will go here
+
+      <div>Lines
+        {lines.map((line) => (
+          <div key={line.id}>
+            <b>line {line.id}</b>
+            {line.customers.map((customer) => (
+              <CustomerInLine customer={customer} />
+            ))}
+          </div>
+        ))}
+      </div>
+
+      <div>
+        {customersWaitingToJoinLine.length} waiting customers
+      </div>
+    </div>
+  );
+}
+
+export default App;
