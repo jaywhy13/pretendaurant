1168c3f12ef1a615baf90e481b36f5690a3151a6
Jean-Mark Wright <jeanmark.wright@gmail.com>
2021-08-31 22:36:37 -0400

    Stopped storing objects within objects, use ID references instead
    Instead of storing the cashier and customer object on the line, use the IDs to
    reference the object instead. This is more consistent with what we're expecting
    to see returned from an API.

--- src/types.ts
+++ src/types.ts
@@ -10,6 +10,7 @@ export interface Customer {
 
 export interface Line {
     id: string;
-    cashier?: Cashier;
-    customers: Customer[];
+    cashierId?: string;
+    customerIds: string[];
 }
+
