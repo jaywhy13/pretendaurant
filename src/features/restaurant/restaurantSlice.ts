fc7de0f01a9843a95cd79c18b51b5e818fc5005c
Jean-Mark Wright <jeanmark.wright@gmail.com>
2022-04-03 20:54:56 -0400

    Simplified line assignments
    There were different actions that resulted in the line being updated. I
    simplified this so there's only one lineUpdated action we're reacting to and
    updating the line.

--- src/features/restaurant/restaurantSlice.ts
+++ src/features/restaurant/restaurantSlice.ts
@@ -1,49 +1,25 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { Customer, Line } from "../../types";
+import { Line } from "../../types";
 
 export interface restaurantState {
-    customersWaitingToJoinLine: Customer[];
     lines: Line[];
 }
 
 const initialState: restaurantState = {
-    customersWaitingToJoinLine: [],
     lines: []
 };
-
-export interface CustomerLineAssignment {
-    customer: Customer;
-    line: Line;
-}
 
 const restaurantSlice = createSlice({
     name: 'restaurant',
     initialState,
     reducers: {
-        customersAdded: (state: restaurantState, action: PayloadAction<Customer[]>) => {
-            console.log("Adding customers to waiting list", action.payload);
-            state.customersWaitingToJoinLine = state.customersWaitingToJoinLine.concat(action.payload);
+        linesUpdated: (state: restaurantState, action: PayloadAction<Line[]>) => {
+            state.lines = [...action.payload];
         },
-        linesAdded: (state: restaurantState, action: PayloadAction<Line[]>) => {
-            state.lines = action.payload;
-        },
-        addCustomerToLine: (state: restaurantState, action: PayloadAction<CustomerLineAssignment>) => {
-            const { customer, line } = action.payload;
-            console.log("Adding", customer.id, "to", line.id);
-            // Remove the customer from the queue
-            state.customersWaitingToJoinLine = state.customersWaitingToJoinLine.filter(candidateCustomer => candidateCustomer.id !== customer.id);
-            const lineInState = state.lines.find(candidateLine => candidateLine.id === line.id);
-            // Add them to the line
-            const updatedLine = {
-                ...line,
-                customers: lineInState!.customers.concat([customer])
-            };
-            state.lines = state.lines.map(lineInState => lineInState.id === line.id ? updatedLine : lineInState);
-        }
     }
 })
 
-export const { customersAdded, linesAdded, addCustomerToLine } = restaurantSlice.actions;
+export const { linesUpdated } = restaurantSlice.actions;
 
 
 export default restaurantSlice.reducer;
