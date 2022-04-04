af98551abc9b416bec9a4e16f35242fb6bcd00ef
Jean-Mark Wright <jeanmark.wright@gmail.com>
2022-04-03 21:04:22 -0400

    Stop showing number of customers waiting to join the line. Not sure why I stopped doing this

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,9 @@ import { useSelector } from 'react-redux';
 import './App.css';
 import { Clock } from './components/Clock';
 import { CustomerInLine } from './components/CustomerInLine';
-import { selectCustomersWaitingToJoinLine, selectLines } from './features/restaurant/selectors';
 
 function App() {
   const lines = useSelector(selectLines);
-  const customersWaitingToJoinLine = useSelector(selectCustomersWaitingToJoinLine);
   return (
     <div className="App">
       <Clock />
@@ -21,10 +19,6 @@ function App() {
             ))}
           </div>
         ))}
-      </div>
-
-      <div>
-        {customersWaitingToJoinLine.length} waiting customers
       </div>
     </div>
   );
