268d95741d13511ecfe93d609a9aaee4103edd00
Jean-Mark Wright <jeanmark.wright@gmail.com>
2022-04-03 20:58:20 -0400

    Removed unused selectors

--- src/features/restaurant/selectors.ts
+++ src/features/restaurant/selectors.ts
@@ -1,19 +1,6 @@
 import { RootState } from "../../store";
+import { Line } from "../../types";
 
 
-export const selectCustomersWaitingToJoinLine = (state: RootState) => state.restaurant.customersWaitingToJoinLine;
 export const selectLines = (state: RootState): Line[] => state.restaurant.lines;
 
-export const selectEmptiestLine = (state: RootState) => {
-    if (state.restaurant.lines) {
-        let emptiestLine = state.restaurant.lines[0];
-        state.restaurant.lines.forEach(line => {
-            if (line.customers.length < emptiestLine.customers.length) {
-                emptiestLine = line;
-            }
-        })
-        return emptiestLine;
-    }
-
-    return undefined;
-}
