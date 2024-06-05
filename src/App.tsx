31a66d5187de0a0a89551cc8021b93ffcc396602
Jean-Mark Wright <jeanmark.wright@gmail.com>
2024-06-04 23:46:51 -0400

    Formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
-import { useSelector } from 'react-redux';
-import './App.css';
-import { Clock } from './components/Clock';
-import { CustomerInLine } from './components/CustomerInLine';
-import { selectLines } from './features/restaurant/selectors';
+import { useSelector } from "react-redux";
+import "./App.css";
+import { Clock } from "./components/Clock";
+import { CustomerInLine } from "./components/CustomerInLine";
+import { selectLines } from "./features/restaurant/selectors";
+import { CustomerInLine as LocalCustomerInLine } from "./types";
 
 function App() {
   const lines = useSelector(selectLines);
+
   return (
     <div className="App">
       <Clock />
       Everything will go here
-
-      <div>Lines
+      <div>
+        Lines
         {lines.map((line) => (
           <div key={line.id}>
-            <b>line {line.id}</b>{line.cashierId ? <span> w/ cashier - {line.cashierId}</span> : null}
-            {line.customerIds.map((customerId) => (
-              <CustomerInLine customerId={customerId} key={customerId} />
+            <b>line {line.id}</b>
+            {line.cashierId ? <span> w/ cashier - {line.cashierId}</span> : null}
+            {line.customersInLine.map(({ customer: { id } }) => (
+              <CustomerInLine customerId={id} key={id} />
             ))}
           </div>
         ))}
